fcf2bfb6d4e42656ab08a6332f72a760b3a4b56f
Scott Murray <scott@murrsco.net>
2018-05-06 09:58:35 -0400

    Created seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+    Blog.create!(
+        title: Faker::Lorem.sentence,
+        body: Faker::Lorem.paragraph
+    )
+end
+
+5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 15
+    )
+end
+
+9.times do |profolio_item|
+    Portfolio.create!(
+        title: "Title: House #{Faker::GameOfThrones.house}",
+        subtitle: Faker::Lorem.sentence,
+        body: Faker::Lorem.paragraph,
+        main_image: "http://via.placeholder.com/600x400",
+        thumb_image: "http://via.placeholder.com/350x200", 
+    )
+end
