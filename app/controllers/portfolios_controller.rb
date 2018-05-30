7be44a78bc6a637e264ba0f977da087bba63fd6e
Scott Murray <scott@murrsco.net>
2018-05-30 17:10:05 -0400

    Implemented show functionality for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -36,7 +36,7 @@ class PortfoliosController < ApplicationController
       end
 
     def show
-        @portfolio_item = Portfolio.find(:id)
+        @portfolio_item = Portfolio.find(params[:id])
     end
 
     def destroy
